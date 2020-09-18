2d5926139c44922b3d4fa72beff6abd9973806e8
Googler <noreply@google.com>
2020-09-18 10:08:55 -0700

    Add support for BluetoothLeAdvertiser in ShadowBluetoothAdapter.
    PiperOrigin-RevId: 332473499

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothAdapter.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothAdapter.java
@@ -12,6 +12,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.content.Context;
 import android.os.Build;
@@ -40,6 +41,7 @@ public class ShadowBluetoothAdapter {
 
   private static boolean isBluetoothSupported = true;
   private static BluetoothLeScanner bluetoothLeScanner = null;
+  private static BluetoothLeAdvertiser bluetoothLeAdvertiser = null;
 
   private Set<BluetoothDevice> bondedDevices = new HashSet<BluetoothDevice>();
   private Set<LeScanCallback> leScanCallbacks = new HashSet<LeScanCallback>();
@@ -61,6 +63,7 @@ public class ShadowBluetoothAdapter {
   public static void reset() {
     setIsBluetoothSupported(true);
     bluetoothLeScanner = null;
+    bluetoothLeAdvertiser = null;
   }
 
   @Implementation
@@ -88,6 +91,15 @@ public class ShadowBluetoothAdapter {
     }
 
     return bluetoothLeScanner;
+  }
+
+  @Implementation(minSdk = LOLLIPOP)
+  protected BluetoothLeAdvertiser getBluetoothLeAdvertiser() {
+    return bluetoothLeAdvertiser;
+  }
+
+  public void setBluetoothLeAdvertiser(BluetoothLeAdvertiser advertiser) {
+    bluetoothLeAdvertiser = advertiser;
   }
 
   @Implementation
